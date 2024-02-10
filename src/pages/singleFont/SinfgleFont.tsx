c1eb036ad38fd3999a7a4af5f2125808adabe8c5
GITHUBDAGI <129954447+watshihaharuki@users.noreply.github.com>
2024-02-10 22:55:11 +0500

    some problems fixed

--- src/pages/singleFont/SinfgleFont.tsx
+++ src/pages/singleFont/SinfgleFont.tsx
@@ -1,25 +1,63 @@
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import './SingleFonts.scss'
 import { Container } from "../../utils/Utils";
 import Navbar from "../../components/navbar/Navbar";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import TextField from '@mui/material/TextField';
+
 interface SingleProps {
   search: String,
   setSearch: React.Dispatch<React.SetStateAction<String>>,
 }
 const SinfgleFont:React.FC<SingleProps> = ({search, setSearch}) => {
   const {name} = useParams();
+  const navigate = useNavigate();
+  const API_KEY = "AIzaSyD7R0Z4PmibH-7aVLDWrfNRiXiyvOss40Q"
+  const [currentFont, setCurrentFont] = useState([]);
+  const [fontSize, setFontSize] = useState<number>(48)
+  const [exampletext, setExamletext] = useState<string>("Whereas recognition of the inherent dignity")
   useEffect(()=>{
-    
+    search.length > 0 && navigate("/")
   },[search])
-  return (
-    <>
+  useEffect(() => {
+    fetch(`https://www.googleapis.com/webfonts/v1/webfonts?key=${API_KEY}&family=${name}`)    
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          setCurrentFont(data.items[0].variants)
+        })
+    },[])
+    // console.log(currentFont.variants)
+    return (
+    currentFont && <>
       <Navbar search={search} setSearch={setSearch}/>
       <Container>
         <div className="single-font-wrapper">
             <h4 className="single-font-title">{name}</h4>
             <p style={{fontSize:"55px", lineHeight:"65px", marginTop:"50px", textAlign:"center", fontFamily:name + ' regular'}}>Whereas disregard and contempt for human rights have resulted</p>
         </div>
+        <h5 style={{fontFamily:"googlesans", fontSize:"30px", marginTop:"60px"}}>Styles</h5>
+        <div className="single-filter-wrapper">
+            <TextField sx={{width:"70%"}} id="outlined-basic" label="Type here to preview text" variant="outlined" onChange={(e:React.ChangeEvent<HTMLInputElement>) => {e.target.value.length > 0 ? setExamletext(e.target.value) : setExamletext("Whereas recognition of the inherent dignity")}}/>
+            <div className="slider-container">
+              <p style={{fontSize:"18px", width:"40px"}}>{fontSize}</p>
+              <input value={fontSize} type="range" min="8" max="100" onChange={(e:React.ChangeEvent<HTMLInputElement>) => setFontSize(e.target.value)} className="slider" id="mySlider"/>
+            </div>
+        </div>
+        <div className="single-font-variants">
+              {
+                currentFont.map((variant:string) => {
+                  return <div className="font-variant-wrapper">
+                      <p>{name + ' '}{variant.includes('0') ? variant.slice(0,3) + ' ' + variant.slice(3, variant.length) : variant} </p>
+                      {variant.includes('italic') ? 
+                      <em style={{margin:"25px 0 40px 0",fontFamily:name +  ' ' + variant, fontWeight:variant.length > 3 ? variant.slice(0,3) : variant, fontSize:`${fontSize}px`}}>{exampletext}</em>
+                      :
+                      <p style={{margin:"25px 0 40px 0",fontFamily:name +  ' ' + variant, fontWeight:variant.length > 3 ? variant.slice(0,3) : variant, fontSize:`${fontSize}px`}}>{exampletext}</p>
+                     }
+                  </div>
+                })
+              }
+            </div>
       </Container>
     </>
   )
